19b2c8d538006c7d7e2c8a2eb39f8dd0053feb46
wordstosong <johnja14@gmail.com>
2017-02-09 02:18:18 -0600

    Removed some whitespace

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.use(expressServer.static('public'));
 
 var defaultPort = 8081;
 var port = process.env.PORT || defaultPort;
-
 app.listen(port, function() {
     console.log('server is listening on port ' + port);
 });
