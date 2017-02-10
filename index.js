222eeb1fe1e7d0d282969c2d65f55d18899ac407
wordstosong <johnja14@gmail.com>
2017-02-09 22:36:18 -0600

    changed port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = expressServer();
 
 app.use(expressServer.static('public'));
 
-var defaultPort = 8081;
+var defaultPort = 9239;
 
 var port = process.env.PORT || defaultPort;
 app.listen(port, function() {
