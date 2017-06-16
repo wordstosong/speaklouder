ba077f5c5673f5fb0c5569c7e78de03cdcc7aea6
mherodev <mherold@fandango.com>
2017-06-16 00:20:40 -0700

    #15 - Went crazy with partials and rendering data

--- app.js
+++ app.js
@@ -4,21 +4,26 @@ var expressServer = require('express');
 
 var app = expressServer();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: './views/layouts',
+    partialsDir: './views'
+}));
 app.set('view engine', 'handlebars');
 
+var title = 'Speak Louder!';
+
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', { title: title });
 });
 
 app.get('/about', function (req, res) {
-    res.render('about');
+    res.render('about', { title: title + ' | About' });
 });
 
 app.use(expressServer.static('public'));
 
 var defaultPort = 8081;
-
 
 var port = process.env.PORT || defaultPort;
 app.listen(port, function() {
